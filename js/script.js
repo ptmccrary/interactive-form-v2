245f709135cc48fb61c3b50076bc5c64ebb87b7b
Parker McCrary <parkertomm3@gmail.com>
2020-06-14 17:41:10 -0400

    Show/hide color based on design select

--- js/script.js
+++ js/script.js
@@ -6,14 +6,47 @@ document.getElementById('name').focus();
  *  When other is checked, text box appears
 ***/
 const otherJob = document.getElementById('other-title');
-otherJob.style.display = 'none';
+otherJob.hidden = true;
 const jobSelect = document.getElementById('title');
 
-jobSelect.addEventListener('click', () => {
-    if(jobSelect.value === 'other') {
-        otherJob.style.display = 'block';
+jobSelect.addEventListener('input', (e) => {
+    if(e.target.value === 'other') {
+        otherJob.hidden = false;
     }else {
-        otherJob.style.display = 'none'
+        otherJob.hidden = true;
     }
 });
 
+/***
+ * T-Shirt Section
+***/
+function hideColorSelect() {
+    const colorSelect = document.getElementById('color');
+    colorSelect.hidden = true;
+    const colorLabel = document.querySelector('#colors-js-puns label');
+    colorLabel.textContent = 'Please select a T-shirt theme';
+}
+
+function showColorSelect() {
+    const colorSelect = document.getElementById('color');
+    colorSelect.hidden = false;
+    const colorLabel = document.querySelector('#colors-js-puns label');
+    colorLabel.textContent = 'Colors:'
+}
+
+const designSelect = document.getElementById('design');
+const colorOptions = document.getElementById('color').children;
+
+
+designSelect.addEventListener('input', (e) => {
+    if(e.target.value === 'js puns'){
+        showColorSelect();
+        console.log('hi');
+    }else if(e.target.value === 'heart js'){
+        showColorSelect();
+        console.log('yo');
+    }else {
+        hideColorSelect();
+        console.log('sup');
+    }
+})
